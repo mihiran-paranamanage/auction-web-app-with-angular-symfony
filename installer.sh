4146c414a6e7ae99028d23f26c2483667397d5cd
mihiran-paranamana <mihiran.hlrm@gmail.com>
2021-06-16 13:22:47 +0530

    Configure dev environment

--- installer.sh
+++ installer.sh
@@ -2,6 +2,11 @@
 
 echo "Installing . . . ";
 
+# Adding php src files
+sudo rm -rf ./php/src/*
+sudo cp -R ./server/composer.json ./php/src/
+sudo cp -R ./server/composer.lock ./php/src/
+
 # Running docker build and up commands in order to build docker images and up the containers.
 sudo docker-compose build
 sudo docker-compose up -d
