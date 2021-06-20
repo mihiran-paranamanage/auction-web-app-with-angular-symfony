bb7504f232592d7022189145707710a19274272f
mihiran-paranamana <mihiran.hlrm@gmail.com>
2021-06-21 01:09:21 +0530

    unit tests

--- server/tests/Utility/AutoBidManagerTest.php
+++ server/tests/Utility/AutoBidManagerTest.php
@@ -14,21 +14,7 @@ final class AutoBidManagerTest extends TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $bid = new Bid();
-        $autoBidManagerMock->expects($this->never())
-            ->method('saveBid');
         $result = $autoBidManagerMock->autoBid($bid);
         $this->assertEquals(null, $result);
-    }
-
-    public function testSaveBid(): void
-    {
-        $autoBidManagerMock = $this->getMockBuilder(AutoBidManager::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $bid = new Bid();
-        $autoBidManagerMock->expects($this->never())
-            ->method('autoBid');
-        $result = $autoBidManagerMock->saveBid($bid);
-        $this->assertEquals(true, $result instanceof Bid);
     }
 }
